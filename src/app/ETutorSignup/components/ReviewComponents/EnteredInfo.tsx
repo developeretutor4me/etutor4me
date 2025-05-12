52d4f33a6effd91ac9ba24536b862cd616ebe6ce
Abdullah <theabdullah224@gmail.com>
2025-05-12 22:26:45 +0500

    Fixes:T024,T026,T027,T028,T029

--- src/app/ETutorSignup/components/ReviewComponents/EnteredInfo.tsx
+++ src/app/ETutorSignup/components/ReviewComponents/EnteredInfo.tsx
@@ -1,23 +1,35 @@
-import React from 'react'
+import React from "react";
 
-interface InformationProps  {
-    name?:string,
-    info?:string,
-    info2?:string,
-    info3?:string,
-    info4?:string,
-    span?:string
+interface InformationProps {
+  name?: string;
+  info?: string;
+  info2?: string;
+  info3?: string;
+  info4?: string;
+  span?: string;
+  EditActive?: boolean;
 }
-const EnteredInfo:React.FC<InformationProps> = ({name,info,span,info2,info3,info4}:any) => {
+const EnteredInfo: React.FC<InformationProps> = ({
+  name,
+  info,
+  span,
+  info2,
+  info3,
+  info4,
+  EditActive,
+}: any) => {
   return (
-    <div className='text-lg custom-xl:text-[27px] text-darkBlue' >
-      <h2  className=' font-semibold' >{name}<span className='font-light' >{span}</span></h2>
-      <p className='mt-2 font-light'  >{info}  </p>
-      <p className='mt-2 font-light'  >{info2}  </p>
-      <p className='mt-2 font-light'  >{info3}  </p>
-      <p className='mt-2 font-light'  >{info4}  </p>
+    <div className="text-lg custom-xl:text-[27px] text-darkBlue">
+      <h2 className={`${EditActive ? "font-normal" : "font-semibold"}`}>
+        {name}
+        <span className="font-light">{span}</span>
+      </h2>
+      <p className="mt-2 sm:mt-5 font-light">{info} </p>
+      <p className="mt-2 sm:mt-5 font-light">{info2} </p>
+      <p className="mt-2 sm:mt-5 font-light">{info3} </p>
+      <p className="mt-2 sm:mt-5 font-light">{info4} </p>
     </div>
-  )
-}
+  );
+};
 
-export default EnteredInfo
+export default EnteredInfo;
--- src/app/ETutorSignup/components/ReviewFormHead.tsx
+++ src/app/ETutorSignup/components/ReviewFormHead.tsx
@@ -1,12 +1,17 @@
-'use state'
-import React, { useState } from 'react';
-import Image from 'next/image';
+"use state";
+import React, { useState } from "react";
+import Image from "next/image";
 import edit from "../../../../public/assets/icons/editicon.svg";
 // import save from "../../../../public/assets/icons/saveicon.svg"; // Add the save icon
 // import cancel from "../../../../public/assets/icons/cancelicon.svg"; // Add the cancel icon
-import FormHeading from './FormHeading';
+import FormHeading from "./FormHeading";
 
-const ReviewFormHead = ({ heading,handleEditToggle,EditActive }:any) => {
+const ReviewFormHead = ({
+  heading,
+  handleEditToggle,
+  EditActive,
+  containerclass,
+}: any) => {
   const [isEditing, setIsEditing] = useState(false);
 
   const handleEditClick = () => {
@@ -24,32 +29,46 @@ const ReviewFormHead = ({ heading,handleEditToggle,EditActive }:any) => {
   };
 
   return (
-    <div className={`flex justify-between pb-2 custom-xl:pb-4 custom-xl:pl-5 items-center border-b ${isEditing ? 'border-customBlue' : 'border-darkBlue'} `}>
+    <div
+      className={`flex justify-between  py-4 custom-xl:pb-5 custom-xl:pl-5 items-center border-b-2   custom-xl:pt-[44px]  ${containerclass}  ${
+        EditActive ? "border-[#b698ff]" : "border-[#c1b8e1]"
+      } `}
+    >
       <FormHeading heading={heading} />
-      <div className='flex gap-2'>
+      <div className="flex gap-2 pr-4">
         {EditActive ? (
-          <div className='flex custom-xl:font-bold items-center gap-3 custom-xl:gap-8' >
-            <button 
-              className='flex items-center gap-2 '
-            onClick={()=>handleEditToggle()}
+          <div className="flex custom-xl:font-bold items-center gap-3 custom-xl:gap-8">
+            <button
+              className="flex items-center gap-2 "
+              onClick={() => handleEditToggle()}
             >
-              <span  className='text-sm custom-xl:text-[20px] text-customBlue '>Cancel</span>
+              <span className="text-sm custom-xl:text-[20px] text-customBlue ">
+                Cancel
+              </span>
             </button>
-            <button 
-              className='flex items-center justify-center gap-2 bg-customBlue custom-xl:px-0 px-6 custom-xl:py-0 py-1.5 custom-xl:w-28  custom-xl:h-14 rounded-full'
-              onClick={()=>handleEditToggle()}
+            <button
+              className="flex items-center justify-center gap-2 bg-customBlue custom-xl:px-0 px-6 custom-xl:py-0  custom-xl:w-28  custom-xl:h-12 rounded-full"
+              onClick={() => handleEditToggle()}
             >
-            
-              <span className='text-sm custom-xl:text-[20px] text-white '>Save</span>
+              <span className="text-sm custom-xl:text-[20px] text-white ">
+                Save
+              </span>
             </button>
           </div>
         ) : (
-          <button 
-            className='flex items-center gap-1 custom-xl:gap-2 cursor-pointer'
-            onClick={()=>handleEditToggle()}
+          <button
+            className="flex items-center gap-1 custom-xl:gap-2 cursor-pointer custom-xl:h-12"
+            onClick={() => handleEditToggle()}
           >
-            <Image  loading="lazy"  src={edit} alt='Edit' className='w-3 custom-xl:w-5' />
-            <span className='text-sm custom-xl:text-[22px] text-customBlue underline'>Edit</span>
+            <Image
+              loading="lazy"
+              src={edit}
+              alt="Edit"
+              className="w-3 custom-xl:w-5"
+            />
+            <span className="text-sm custom-xl:text-[22px] text-customBlue underline">
+              Edit
+            </span>
           </button>
         )}
       </div>
